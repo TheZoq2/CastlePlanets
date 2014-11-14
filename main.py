59a49960187aaa990b865a23b58219f42352709a
Emil Segerbäck <emilsegerback@gmail.com>
2014-11-14 22:33:41 +0100

    added planets

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import pygame
 from pygame.locals import *
+from planet import *
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 700))
+clock = pygame.time.Clock()
+
+planets = [Planet(640, 350, 0)]
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
             running = False
+
+    for planet in planets:
+        planet.draw(screen)
+
+    pygame.display.flip()
---
+++ planet.py
@@ -0,0 +1,13 @@
+import pygame
+
+planetImgs = [pygame.image.load("planet.png")]
+
+class Planet:
+    def __init__(self, x, y, type):
+        self.x = x
+        self.y = y
+        self.type = type
+
+    def draw(self, screen):
+        screen.blit(planetImgs[self.type], (self.x, self.y))
+
