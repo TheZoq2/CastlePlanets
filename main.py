5acab8c207bdd4b1b1087701fc4ef28516b31c55
Emil Segerbäck <emilsegerback@gmail.com>
2014-11-14 23:10:00 +0100

    added rockets and trade routes

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import pygame
 from pygame.locals import *
 from planet import *
+from trade import *
+from rocket import *
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 700))
 clock = pygame.time.Clock()
 
 planets = [Planet(640, 350, 0)]
+rockets = [Rocket(planets[0])]
+routes = []
 
 running = True
 while running:
@@ -17,4 +21,11 @@ while running:
     for planet in planets:
         planet.draw(screen)
 
+    for traderoute in routes:
+        pass
+
+    for rocket in rockets:
+        rocket.draw(screen)
+
     pygame.display.flip()
+
---
+++ trade.py
@@ -0,0 +1,9 @@
+import pygame
+
+class Traderoute:
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+
+    def draw(self, screen):
+        pass
