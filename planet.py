07980bc6615dfa8183a4fd7fbe7f3bd7272987af
Daniel Persson Proos <Daniel@Daniels-MacBook-Air.local>
2014-11-14 23:39:55 +0100

    added get_coors function for planet class

--- planet.py
+++ planet.py
@@ -3,11 +3,17 @@ import pygame
 planetImgs = [pygame.image.load("planet.png")]
 
 class Planet:
-    def __init__(self, x, y, type):
+    def __init__(self, x, y, type, amount):
         self.x = x
         self.y = y
         self.type = type
 
+
+
     def draw(self, screen):
         screen.blit(planetImgs[self.type], (self.x, self.y))
 
+    def get_coords(self):
+        return (self.x, self.y)
+
+    def get
