313f209b79561fbc8f6a391e224df8ac00436e46
Christoffer <chsnot@gmail.com>
2014-11-15 23:41:59 +0100

    fixed tabs

--- trade.py
+++ trade.py
@@ -12,9 +12,9 @@ class Traderoute:
             renderer.line(self.path[i].get_coords(), self.path[i+1].get_coords())
 
     def routable_planets(source_planet, planets):
-    	close_planets = []
-    	for planet in planets:
-			if getDistance(source_planet.get_coords(), planet.get_coords()):
-				close_planets.append(planet)
+        close_planets = []
+        for planet in planets:
+            if getDistance(source_planet.get_coords(), planet.get_coords()):
+                close_planets.append(planet)
 
-		return close_planets
+        return close_planets
