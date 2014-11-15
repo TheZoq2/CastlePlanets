52ab44382065b4b5319b2329d331b360cb73ba0b
Emil Segerbäck <emilsegerback@gmail.com>
2014-11-15 18:58:04 +0100

    readded space to go back to Earth

--- main.py
+++ main.py
@@ -75,6 +75,8 @@ while running:
                 xscroll = -1
             elif event.key == K_d:
                 xscroll = 1
+            elif event.key == K_SPACE:
+                renderer.camera = Vec2(0, 0)
         if event.type == KEYUP:
             if event.key == K_w or event.key == K_s:
                 yscroll = 0
