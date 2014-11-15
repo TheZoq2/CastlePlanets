cb7ae3a0bd9cdb86839b0f50acf24eabb7d34593
Christoffer <chsnot@gmail.com>
2014-11-15 23:40:21 +0100

    routable_planets

--- trade.py
+++ trade.py
@@ -1,4 +1,5 @@
 import pygame
+from Vec2 import *
 
 class Traderoute:
     def __init__(self, path, cargo):
@@ -9,3 +10,11 @@ class Traderoute:
     def draw(self, renderer):
         for i in range(len(self.path) - 1):
             renderer.line(self.path[i].get_coords(), self.path[i+1].get_coords())
+
+    def routable_planets(source_planet, planets):
+    	close_planets = []
+    	for planet in planets:
+			if getDistance(source_planet.get_coords(), planet.get_coords()):
+				close_planets.append(planet)
+
+		return close_planets
